bd5c037211963a3b421b71aba6965cf234afec5c
Aniket <rajeshwarkumar9204@gmail.com>
2022-10-16 22:48:11 +0530

    add client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,37 @@
-import Nav from './components/Nav';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import SignUp from './components/SignUp'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import './App.css';
+import React from 'react';
+import Nav from './components/Nav'
+import { useState } from 'react';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
+import SignUp from './components/SignUp';
+import Footer from './components/Footer';
+import {BrowserRouter, Route , Routes } from 'react-router-dom'
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-                  <Nav></Nav>
-             <Routes>
-                 <Route path='/' element={<Home></Home>}></Route>
-                 <Route path='/about' element={<About></About>}></Route>
-                 <Route path='/contact' element={<Contact></Contact>}></Route>
-                 <Route path='/signUp' element={<SignUp></SignUp>}></Route>
-             </Routes>
-             <Footer></Footer>
-        </BrowserRouter>
-    </div>
-  );
+ 
+ return(
+        <React.Fragment>
+           <BrowserRouter>
+                <Nav />
+                <Routes>
+                   <Route path='/' element={<Home></Home>} exact ></Route>
+                   <Route path='/about' element={<About></About>} ></Route>
+                   <Route path='/contact' element={<Contact></Contact>} ></Route>
+                   <Route path='/signUp' element={<SignUp></SignUp>} ></Route>
+                </Routes>
+                <Footer/>
+           </BrowserRouter>
+        </React.Fragment>
+   )
+
+ 
 }
 
 export default App;
+ 
